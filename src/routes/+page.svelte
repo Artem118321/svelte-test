30fcbc01921277427d8173db489627c6f70adab4
Artem <ignatovartem0006@gmail.com>
2024-03-27 18:47:07 +0500

    ade

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,21 +12,21 @@
 
     <div class="column">
         <div class="centr">
-            <img style="text-align: center;"  src="svelte-test/img/11.webp" alt="" height="120px" width="100px">
+            <img style="text-align: center;"  src="/svelte-test/img/11.webp" alt="" height="120px" width="100px">
         </div>
       <div class="card"><a href="/">Тенерифе, Испания</a> <br>
         Это самое популярное курортное место в составе Канарских островов Испании. Тенерифе омывается водами Атлантического океана, и круглый год температура воздуха здесь не опускается ниже +21 ⁰С.</div>
     </div>
     <div class="column">
         <div class="centr">
-            <img style="text-align: center;"  src="svelte-test/img/2.webp" alt="" height="120px" width="100px">
+            <img style="text-align: center;"  src="/svelte-test/img/2.webp" alt="" height="120px" width="100px">
         </div>
       <div class="card"><a href="/">Сен-Тропе, Франция</a> <br>
         В список самых известных курортов Европы входит Сен-Тропе на Лазурном Берегу Франции. Небольшой элитный город находится на побережье Средиземного моря.</div>
     </div>
     <div class="column">
         <div class="centr">
-            <img style="text-align: center;"  src="svelte-test/img/3.webp" alt="" height="120px" width="100px">
+            <img style="text-align: center;"  src="/svelte-test/img/3.webp" alt="" height="120px" width="100px">
         </div>
       <div class="card"><a href="/">Пафос, Кипр</a> <br>
         Самый дорогой курорт на острове Кипр популярен среди туристов. Здесь расположены лучшие пятизвёздочные отели, престижные рестораны и пляжи.</div>
@@ -36,21 +36,21 @@
       <div class="row">
         <div class="column">
             <div class="centr">
-                <img style="text-align: center;"  src="svelte-test/img/4.webp" alt="" height="120px" width="100px">
+                <img style="text-align: center;"  src="/svelte-test/img/4.webp" alt="" height="120px" width="100px">
             </div>
           <div class="card"><a href="/">Рио-де-Жанейро, Бразилия</a>  <br>
             На крупном оживлённом курорте находятся лучшие пляжи Бразилии: Копакабана, Леблон и Ипанема.</div>
         </div>
         <div class="column">
             <div class="centr">
-                <img style="text-align: center;"  src="svelte-test/img/5.webp" alt="" height="120px" width="100px">
+                <img style="text-align: center;"  src="/svelte-test/img/5.webp" alt="" height="120px" width="100px">
             </div>
           <div class="card"><a href="/">Маэ, Сейшельские Острова</a> <br>
             Здесь находится столица Виктория и более 60 песчаных пляжей с белым песком и тропической растительностью.</div>
         </div>
         <div class="column">
             <div class="centr">
-                <img style="text-align: center;"  src="svelte-test/img/6.webp" alt="" height="120px" width="100px">
+                <img style="text-align: center;"  src="/svelte-test/img/6.webp" alt="" height="120px" width="100px">
             </div>
           <div class="card"><a href="/">Гранд-Бэй, Маврикий</a> <br>
             К самым живописным курортам Маврикия относится небольшой город Гранд-Бэй, расположенный в бухте на севере острова.</div>
